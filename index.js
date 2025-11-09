db8717d3b97148219dce36ad10282c788968f9b0
ZahidHasan71 <zahidhasan0000001@gmail.com>
2025-11-09 20:40:43 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 5000
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Server is running')
